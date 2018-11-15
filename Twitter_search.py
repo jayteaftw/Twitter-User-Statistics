59693bd144f6092d6135521403269bc69906a048
jayteaftw <jtvinolus@gmail.com>
2018-11-14 21:41:45 -0800

    Added input to choose which twitter use and how many pages of tweets to use. Add options which calculate avergae likes, retweets, or comments per tweets.

--- Twitter_search.py
+++ Twitter_search.py
@@ -6,14 +6,16 @@ from pyppeteer import launch
 
 async def main():
     on = 1
-    user = input('Enter Twitter User')
-    print("Loading Data")
+    user = input('Enter Twitter User: ')
+    print("Loading User")
+    user = 'https://twitter.com/' + user
     browser = await launch(headless=True)
     page = await browser.newPage()
-    await page.goto('https://twitter.com/elonmusk')
+    await page.goto(user)
     await asyncio.sleep(2)
 
-    pages = 10
+    pages = int(input('How many pages?: '))
+    print("Loading Pages")
     for i in range(pages):
         await page.evaluate('() => window.scrollTo(0,document.body.scrollHeight)')
         await asyncio.sleep(1)
@@ -38,22 +40,38 @@ async def main():
         # Commit the data we got from this tweet to our list. We use a tuple here because of convension.
         tweets.append((name_value, comment_count, retweet_count, like_count))
 
-
-    print(tweets[0])
-
+    length = len(tweets)
+    print(length, 'tweets loaded')
     await browser.close()
     print('Data Loaded')
 
     while(on == 1):
-        option = input("Input 1 for Average likes per tweet, 2 for Average retweet per tweet, 3 for Average comments per tweet or 9 to quit system.")
-        if(option == 1):
+        option = input("Input 1 for Average likes per tweet, 2 for Average retweets per tweet, 3 for Average comments per tweet or 9 to quit system.")
+        if(option == '1'):
             sum = 0
-        elif(option == 2):
+            for i in range(0 , length):
+                sum += float(tweets[i][1][:-1])
+            avg = sum/length
+            print('Average amount of likes per tweet',avg)
+
+        elif(option == '2'):
             sum = 0
-        elif(option == 3):
+            for i in range(0 , length):
+                sum += float(tweets[i][2][:-1])
+            avg = sum/length
+            print('Average amount of retweets per tweet',avg)
+
+        elif(option == '3'):
             sum = 0
-        elif(option == 9):
+            for i in range(0 , length):
+                sum += float(tweets[i][3][:-1])
+            avg = sum/length
+            print('Average amount of comments per tweet',avg)
+
+        elif(option == '9'):
+            print('Closing system.')
             on = 0
+            
         else:
             print('Thats not an option')
            
